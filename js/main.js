e44537d0eb1614a51e1a18caa7c26ea5becef76f
Júlio Cézar <jc.amorim.n01@gmail.com>
2023-07-20 21:53:04 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+    $('form').validate({
+        rules: {
+            nome: {
+            required: true
+            },
+            email: {
+            required: true
+            },
+            telefone: {
+            required: true
+            },
+            cpf: {
+            required: true
+            },
+            enderecoCompleto: {
+            required: true
+            },
+            cep: {
+            required: true
+            }
+        },
+        submitHandler: function(form) {
+        
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+            }
+        }
+    })
+})
